34263db22a16b13fa19a1fa15943ec158e2be788
rbnschlz <rbnschlz@gmail.com>
2016-09-17 13:14:14 +0100

    Cleaning
    Cleaning out the Starter Kit

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+(function($) {
+
+	var example = function(){
+	}
+
+	//document ready
+	$(document).ready(function(){
+
+	});
+
+
+
+	//on load
+	$(window).load(function(){
+		
+	});
+
+	//keypresses
+	$(document).keydown(function(e) {
+	    switch(e.which) {
+	        case 37: // left
+	        break;
+
+	        case 38: // up
+	        case 33: // page up
+	        break;
+
+	        case 39: // right
+	        break;
+
+	        case 40: // down
+	        case 34: // page down
+	        break;
+
+	        case 36: // home
+	        break;
+
+	        case 35: // end
+			break;
+
+	        default: return; // exit this handler for other keys
+	    }
+	    e.preventDefault(); // prevent the default action (scroll / move caret)
+	});
+
+})(jQuery);
