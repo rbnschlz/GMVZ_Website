289878ce66ef7e7b8451e565c9e3eb68bca87d16
rbnschlz <rbnschlz@gmail.com>
2016-09-17 13:05:14 +0100

    Less implementation

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -9,6 +9,7 @@
   <meta name="description" content="<?php echo $site->description()->html() ?>">
   <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
 
+  <?php echo less('assets/less/main.less') ?>
   <?php echo css('assets/css/main.css') ?>
 
 </head>
