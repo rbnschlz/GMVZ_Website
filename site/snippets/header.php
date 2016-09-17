87686b7713cbdc3e6c34dddeed87ad3e7e798bd2
rbnschlz <rbnschlz@gmail.com>
2016-09-17 13:27:01 +0100

    Cleaning

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,23 +1,39 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
+<?php
+// $url = $site->find("archive")->children()->visible()->filter(function($child) {
+//   return $child->hasImages();
+// });
+// $url = $url->shuffle()->first();
+// $images = $url->images()->shuffle();
+// $img = $images->first()->url();
+?>
 <html lang="en">
 <head>
-
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <title><?php echo html($site->title()) ?></title>
 
-  <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
-  <meta name="description" content="<?php echo $site->description()->html() ?>">
-  <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
+  <meta name="description" content="<?php echo html($site->description()) ?>" />
+  <meta name="keywords" content="<?php echo html($site->keywords()) ?>" />
+  <meta name="robots" content="index, follow" />
+  <meta property="og:url" content="<?php echo html($site->url()) ?>/" />
+  <meta property="og:description" content="<?php echo html($site->description()) ?>" />
+  <meta property="og:image" content="<?php //echo $img; ?>"" />
+  <link rel="image_src" href="<?php //echo $img; ?>" />
 
-  <?php echo less('assets/less/main.less') ?>
-  <?php echo css('assets/css/main.css') ?>
+  <?php 
+    echo less('assets/less/main.less');
+    echo css('assets/css/main.css');
+   ?>
+
+  <link rel="apple-touch-icon" sizes="180x180" href="">
+  <link rel="icon" type="image/png" href="" sizes="32x32">
+  <link rel="icon" type="image/png" href="" sizes="16x16">
+  <link rel="manifest" href="">
+  <link rel="mask-icon" href="" color="#144314">
+  <link rel="shortcut icon" href="">
+  <meta name="msapplication-config" content="">
+  <meta name="theme-color" content="#ffffff">
+
 
 </head>
-<body>
-
-  <header class="header cf" role="banner">
-    <a class="logo" href="<?php echo url() ?>">
-      <img src="<?php echo url('assets/images/logo.svg') ?>" alt="<?php echo $site->title()->html() ?>" />
-    </a>
-    <?php snippet('menu') ?>
-  </header>
