7704b7ea6977ebe389b9107089d8919c289219cc
rbnschlz <rbnschlz@gmail.com>
2016-09-18 11:52:55 +0100

    Menu Filter

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,41 +1,63 @@
 <?php snippet('header') ?>
 
 <div class="menu_wrapper">
-<div class="menu_artists">
-	<?php foreach($artists as $artist):
-		$title = $artist->title()->html();
-		$titlelow = $title->lower()->htm();
-		$titlelow = preg_replace('/\s*/', '', $titlelow);
-		$request = kirby()->request()->query('artists');
-		$active = (strpos($request, $titlelow) !== false ) ? 'active' : '';
-			$output = " <a href='?artists=";
-			$output .= $titlelow;
-			$output .="' class='menu_item ";
-			$output .= $active;
-			$output .="'>";
-			$output .= $title;
-			$output .=",</a>";
-			echo $output;
-	endforeach ?>
-</div>
-	<div class="menu_time">
-	<?php 
-	$times = array("Upcoming", "Current", "Past");
-	foreach($times as $time):
-		$timelow = strtolower($time);
-		$request = kirby()->request()->query('time');
-		$active = (strpos($request, $timelow) !== false ) ? 'active' : '';
-			$output = " <a href='?time=";
-			$output .= $timelow;
-			$output .="' class='menu_item ";
-			$output .= $active;
-			$output .="'>";
-			$output .= $time;
-			$output .=",</a>";
-			echo $output;
-	endforeach ?>
+	<div class="menu_artists">
+		<?php foreach($artists as $artist):
+			$title = $artist->title()->html();
+			$titlelow = $title->lower()->htm();
+			$titlelow = preg_replace('/\s*/', '', $titlelow);
+			$request = kirby()->request()->query('artists');
+			$active = (strpos($request, $titlelow) !== false ) ? 'active' : '';
+				$output = " <a href='?artists=";
+				$output .= $titlelow;
+				$output .="' class='menu_item ";
+				$output .= $active;
+				$output .="'>";
+				$output .= $title;
+				$output .=",</a>";
+				echo $output;
+		endforeach ?>
 	</div>
+
+	<div class="menu_time">
+		<?php $times = array("Upcoming", "Current", "Past");
+		foreach($times as $time):
+			$timelow = strtolower($time);
+			$request = kirby()->request()->query('time');
+			$active = (strpos($request, $timelow) !== false ) ? "active" : "";
+			$url = $_SERVER['REQUEST_URI'];
+			if (!isset($_GET["time"]) && !isset($_GET["artists"])) {
+				$param = " <a href='";
+				$param .= $url;
+				$param .= "?time=";
+			} else if (!isset($_GET["time"]) && isset($_GET["artists"])) {
+				$param = " <a href='";
+				$param .= $url;
+				$param .= "&time=";
+			} else if ( isset($_GET["time"])) {
+				$param = " <a href=',";
+			};
+				$output = $param;
+				$output .= $timelow;
+				$output .="' class='menu_item ";
+				$output .= $active;
+				$output .="'>";
+				$output .= $time;
+				$output .=",</a>";
+				echo $output;
+		endforeach ?>
+	</div>
+
 </div>
+
+
+
+
+
+
+
+
+
 
 <div class="main_wrapper">
 
