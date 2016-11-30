082b591118065979e6883ac8606643797959d613
rbnschlz <rbnschlz@gmail.com>
2016-11-30 15:26:10 +0100

    Frontpage

---
+++ site/controllers/home.php
@@ -0,0 +1,48 @@
+<?php
+
+return function($site, $pages, $page) {
+
+	function returnDate($start, $end) {
+    $start_date = explode(" ", date('j M Y', $start));
+    $end_date = explode(" ", date('j M Y', $end));
+    $start_day = $start_date[0];
+    $end_day = $end_date[0];
+    $start_month = $start_date[1];
+    $end_month = $end_date[1];
+    $start_year = $start_date[2];
+    $end_year = $end_date[2];
+
+    $date = "";
+    $span = " – ";
+    $date .= $start_day;
+    if ($end) {
+        if ($start_year == $end_year) {
+            if ($start_month == $end_month) {
+                if ($start_day == $end_day) {
+                    $date .= " ".$start_month." ".$start_year;
+                } else {
+                    $date .= $span.$end_day." ".$start_month." ".$start_year;
+                }
+            } else {
+                $date .= " ".$start_month.$span.$end_day." ".$end_month." ".$end_year;
+            }
+        } else {
+            $date .= " ".$start_month." ".$start_year.$span.$end_day." ".$end_month." ".$end_year;
+        }
+    } else {
+        $date .= " ".$start_month." ".$start_year;
+    }
+        return $date;
+    }
+
+	$shows = page('shows')->children()->sortBy('startdate', 'desc', 'enddate', 'asc')->visible();
+    $current_date = strtotime(date('Y-m-d H:i:s'));
+
+    return array(
+        'current_date' => $current_date,
+        'shows' => $shows,
+    );
+
+};
+
+?>
--- site/templates/home.php
+++ site/templates/home.php
@@ -1,5 +1,49 @@
-<?php go('/shows') ?>
+<?php 
+	$featured = [];
+	$img = "";
+	$capt = "";
+	foreach($shows as $show) {
+	    $start = strtotime($show->startdate());
+	    $end = strtotime($show->enddate());
 
-<?php snippet('header'); snippet('home_menu'); ?>
+	    //Set Date variable
+	    if ($end < $current_date) {
+	        $featured[] = false;
+	    } else if (($start < $current_date) && ($end > $current_date)) {
+	        $featured[] = true;
+	        $img = "style='background-image:url(".$show->images()->first()->url().")'";
+	        $event = $show;
+	    } else if ($start > $current_date) {
+	        $featured[] = false;
+	    };
+	}
 
-<?php snippet('footer') ?>
+	if(in_array(true, $featured)) {
+		snippet('header');
+		echo "<div class='menu_wrapper'>";
+		snippet('site_nav_menu');
+		echo "</div>";
+
+		$block = "<div class='home_background' ";
+		$block .= $img;
+		$block .= "></div>";
+
+		echo $block;
+
+		$block = "<div class='home_caption'><span>Current exhibition: </span><a class='' href='";
+		$block .= $event->url();
+		$block .= "'>";
+		$block .= $event->title();
+		$block .= "</a>";
+		// $block .= "<span>by ";
+		// $block .= $event->artist();
+		$block .= "</div>";
+
+		echo $block;
+
+		snippet('footer');
+
+	} else { 
+		go('/shows'); 
+	}; 
+?>
