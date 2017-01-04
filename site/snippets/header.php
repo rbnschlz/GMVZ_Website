a939853806576fc1fd379dda59adc0c2270714f8
Bram van den Berg <mail@bramvandenberg.com>
2017-01-04 12:03:30 +0100

    Added favicon

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -25,14 +25,12 @@
     echo less('assets/less/main.less');
     echo css('assets/css/main.css');
    ?>
-
-  <link rel="apple-touch-icon" sizes="180x180" href="">
-  <link rel="icon" type="image/png" href="" sizes="32x32">
-  <link rel="icon" type="image/png" href="" sizes="16x16">
-  <link rel="manifest" href="">
-  <link rel="mask-icon" href="" color="#144314">
-  <link rel="shortcut icon" href="">
-  <meta name="msapplication-config" content="">
+  
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" href="assets/favicon/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="assets/favicon/favicon-16x16.png" sizes="16x16">
+  <link rel="manifest" href="assets/favicon/manifest.json">
+  <link rel="mask-icon" href="assets/favicon/safari-pinned-tab.svg" color="#5bbad5">
   <meta name="theme-color" content="#ffffff">
 
 
