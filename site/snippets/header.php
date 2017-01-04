6e668bdcc1accee782bdaf8aa4b818d77206dd2e
rbnschlz <rbnschlz@gmail.com>
2017-01-04 12:32:09 +0100

    Favicon Update

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -26,12 +26,13 @@
     echo css('assets/css/main.css');
    ?>
   
-  <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png">
-  <link rel="icon" type="image/png" href="assets/favicon/favicon-32x32.png" sizes="32x32">
-  <link rel="icon" type="image/png" href="assets/favicon/favicon-16x16.png" sizes="16x16">
-  <link rel="manifest" href="assets/favicon/manifest.json">
-  <link rel="mask-icon" href="assets/favicon/safari-pinned-tab.svg" color="#5bbad5">
-
+  <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/apple-touch-icon.png?v=GvJ77d6nlW">
+  <link rel="icon" type="image/png" href="/assets/img/favicon-32x32.png?v=GvJ77d6nlW" sizes="32x32">
+  <link rel="icon" type="image/png" href="/assets/img/favicon-16x16.png?v=GvJ77d6nlW" sizes="16x16">
+  <link rel="manifest" href="/assets/img/manifest.json?v=GvJ77d6nlW">
+  <link rel="mask-icon" href="/assets/img/safari-pinned-tab.svg?v=GvJ77d6nlW" color="#5bbad5">
+  <link rel="shortcut icon" href="/assets/img/favicon.ico?v=GvJ77d6nlW">
+  <meta name="msapplication-config" content="/assets/img/browserconfig.xml?v=GvJ77d6nlW">
   <meta name="theme-color" content="#ffffff">
 
 
